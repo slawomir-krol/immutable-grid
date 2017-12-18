6c6454c262bfd997431ed3f67a4317413ffbace5
Sławomir Król <slawomir.krol@pl.abb.com>
2017-12-18 14:28:12 +0100

    history based on immutable data structures

--- src/components/GridContainer.js
+++ src/components/GridContainer.js
@@ -4,17 +4,21 @@ import Immutable from "immutable";
 import Grid from "./Grid";
 import History from "./History";
 
-
 class GridContainer extends React.Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
-            grid: this.createGrid(this.props.rows, this.props.columns)
+            grid: this.createGrid(this.props.rows, this.props.columns),
+            history: Immutable.List(),
+            future: Immutable.List()
         };
 
         this.onCellClick = this.onCellClick.bind(this);
+        this.undo = this.undo.bind(this);
+        this.redo = this.redo.bind(this);
+        this.clear = this.clear.bind(this);
     }
 
     createGrid(rows, columns) {
@@ -42,11 +46,37 @@ class GridContainer extends React.Component {
         const immutableGrid = Immutable.fromJS(this.state.grid);
         const newGrid = immutableGrid.updateIn([rowId, 'cells', columnId, 'active'], active => !active).toJS();
 
-        this.updateGrid(newGrid);
+
+        this.setState({
+            grid: newGrid,
+            history: this.state.history.push(this.state.grid)
+        });
     }
 
-    updateGrid(grid) {
-        this.setState({grid});
+    undo() {
+        if (this.state.history.size < 1) return;
+        this.setState({
+            grid: this.state.history.last(),
+            history: this.state.history.pop(),
+            future: this.state.future.push(this.state.grid)
+        });
+    }
+
+    redo() {
+        if (this.state.future.size < 1) return;
+        this.setState({
+            grid: this.state.future.last(),
+            history: this.state.history.push(this.state.grid),
+            future: this.state.future.pop()
+        });
+    }
+
+    clear() {
+        this.setState({
+            grid: this.createGrid(this.props.rows, this.props.columns),
+            history: Immutable.List(),
+            future: Immutable.List(),
+        });
     }
 
     render() {
@@ -54,7 +84,13 @@ class GridContainer extends React.Component {
             <div className="app">
                 <Grid grid={this.state.grid} onCellClick={this.onCellClick}/>
 
-                <History/>
+                <History undo={this.undo}
+                         undoDisabled={this.state.history.size === 0}
+                         redo={this.redo}
+                         redoDisabled={this.state.future.size === 0}
+                         clear={this.clear}
+                         clearDisabled={this.state.history.size === 0 && this.state.future.size === 0}
+                />
             </div>
         );
     }
